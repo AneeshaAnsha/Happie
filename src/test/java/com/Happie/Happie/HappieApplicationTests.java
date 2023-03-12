d91c23d89d7cd07ca84bedc98fe915244e85189f
14759 <aneeshaansha01@gmail.com>
2023-03-12 13:22:34 -0400

    learning dumbu basics - first commit

---
+++ src/test/java/com/Happie/Happie/HappieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Happie.Happie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
